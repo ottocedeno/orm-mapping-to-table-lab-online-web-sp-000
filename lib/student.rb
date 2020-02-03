4358d84cc9a485df99afda40ea457e6b8ff2ea5c
Otto Cedeno <otto@ottostacos.com>
2020-02-03 16:05:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,10 @@
 class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :id, :name
+  attr_accessor :id, :name, :grade
 
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
 end
