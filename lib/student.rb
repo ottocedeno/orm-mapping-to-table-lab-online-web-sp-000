a9723521ddc7eb0b11fab81417ff06cf8e2e9a62
Otto Cedeno <otto@ottostacos.com>
2020-02-03 16:27:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
-    binding.pry
+    
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
