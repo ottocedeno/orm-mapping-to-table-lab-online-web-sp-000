c791438cc52b6054cfef58e87761443c924634fd
Otto Cedeno <otto@ottostacos.com>
2020-02-03 16:23:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?, ?);
     SQL
-    binding.pry
+    # binding.pry
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
