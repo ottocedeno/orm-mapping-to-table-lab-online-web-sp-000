fccd612d55ac158c49c9d6b5f897538d8a630c8d
Otto Cedeno <otto@ottostacos.com>
2020-02-03 16:05:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,8 @@
 class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :id, :name, :grade
+  attr_accessor :name, :grade
+  attr_reader ;id
 
   def initialize(name, grade)
     @name = name
